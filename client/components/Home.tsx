791336cd8f36987cd087d218435a8ba52bb67d80
Quinn Gibson <quinngiibson@gmail.com>
2023-06-20 08:43:45 +1200

    update home component

--- client/components/Home.tsx
+++ client/components/Home.tsx
@@ -32,21 +32,6 @@ function Home() {
         magna sed rhoncus. Suspendisse sit amet nisi odio. Maecenas sem est,
         bibendum laoreet tortor vel, viverra fermentum tortor.
       </p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eget
-        pulvinar risus, non posuere erat. Nam congue dui eget lorem convallis
-        dapibus. Aliquam luctus lectus id risus iaculis, eleifend iaculis diam
-        efficitur. Aliquam risus diam, viverra ut urna nec, gravida pharetra
-        dui. Fusce cursus mauris eu viverra ullamcorper. Nam malesuada felis
-        imperdiet, bibendum nibh in, placerat lorem. Pellentesque condimentum,
-        justo sit amet rutrum hendrerit, diam purus lobortis nulla, eget
-        volutpat turpis metus eu ex. Integer vulputate sollicitudin gravida. In
-        nisi sapien, scelerisque eu luctus nec, accumsan id massa. Morbi ut
-        mollis lacus. Aliquam erat volutpat. Aliquam erat volutpat. Ut mattis
-        velit sapien, eu egestas purus egestas at. Maecenas fringilla tempus
-        magna sed rhoncus. Suspendisse sit amet nisi odio. Maecenas sem est,
-        bibendum laoreet tortor vel, viverra fermentum tortor.
-      </p>
     </>
   )
 }
