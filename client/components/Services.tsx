8ea2afc7a7a298aa304ee9197b984346be4b7bfb
Quinn Gibson <quinngiibson@gmail.com>
2023-06-19 18:52:28 +1200

    add images to services page

--- client/components/Services.tsx
+++ client/components/Services.tsx
@@ -1,9 +1,11 @@
+import { Link } from 'react-router-dom'
 import services from '../../data/serviceData'
 
 function Services() {
   return (
     <>
       <h1 className="page-heading">Services</h1>
+      <p>**2023 no classic or hybrid sets for new clients**</p>
       <div>
         {services.map((service) => {
           return (
@@ -14,7 +16,7 @@ function Services() {
                   <img src={service.img} alt="" className="service-img" />
                 </div>
                 <div className="service-info">
-                  <h2>** {service.name}</h2>
+                  <h2>{service.name}</h2>
                   <h3>
                     {service.time} @ NZ${service.price}.00
                   </h3>
